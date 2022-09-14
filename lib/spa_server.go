b582b9de8f1b5e616744423cb20c836fdc46b5e9
ki3nqt <kienqt98.act@gmail.com>
2022-09-14 20:52:12 +0700

    update packet parser

--- lib/spa_server.go
+++ lib/spa_server.go
@@ -1,9 +1,9 @@
 package lib
 
 import (
+	"errors"
 	"fmt"
 	"log"
-	"strings"
 	"time"
 
 	"github.com/google/gopacket"
@@ -11,7 +11,17 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-type SPAServerHandler struct{}
+// type spaPacketInfor struct {
+// 	srcIPv4  string
+// 	srcIPv6  string
+// 	authData string
+// }
+
+type SPAServerHandler struct {
+	srcIPv4  string
+	srcIPv6  string
+	authData string
+}
 
 func (SPAServerHandler) Init(device string) {
 	fmt.Print("Initing SPA server handler...")
@@ -27,65 +37,46 @@ func (SPAServerHandler) Init(device string) {
 		log.Fatal(err)
 	}
 	defer handle.Close()
-	var filter string = "udp and port 12345"
+	var filter string = "udp and port 62201"
 	err = handle.SetBPFFilter(filter)
 	if err != nil {
 		log.Fatal(err)
 	}
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		printPacketInfo(packet)
+		spaPacketInfor, err := getPacketInfo(packet)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			fmt.Println("Auth data: ", spaPacketInfor.authData)
+		}
 	}
 
 }
 
-func printPacketInfo(packet gopacket.Packet) {
-	// Let's see if the packet is an ethernet packet
-	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
-	if ethernetLayer != nil {
-		fmt.Println("Ethernet layer detected.")
-		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
-		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
-		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
-		// Ethernet type is typically IPv4 but could be ARP or other
-		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
-		fmt.Println()
+func getPacketInfo(packet gopacket.Packet) (SPAServerHandler, error) {
+	spaPacketInfor := SPAServerHandler{}
+	ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
+	if ipv4Layer != nil {
+		ip, _ := ipv4Layer.(*layers.IPv4)
+		fmt.Println("IPv4: ", ip.SrcIP.String())
+		spaPacketInfor.srcIPv4 = ip.SrcIP.String()
+	} else {
+		spaPacketInfor.srcIPv4 = ""
 	}
-
-	// Let's see if the packet is IP (even though the ether type told us)
-	ipLayer := packet.Layer(layers.LayerTypeIPv4)
-	if ipLayer != nil {
-		fmt.Println("IPv4 layer detected.")
-		ip, _ := ipLayer.(*layers.IPv4)
-
-		// IP layer variables:
-		// Version (Either 4 or 6)
-		// IHL (IP Header Length in 32-bit words)
-		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
-		// Checksum, SrcIP, DstIP
-		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
-		fmt.Println("Protocol: ", ip.Protocol)
-		fmt.Println()
+	ipv6Layer := packet.Layer(layers.LayerTypeIPv6)
+	if ipv6Layer != nil {
+		ip, _ := ipv6Layer.(*layers.IPv6)
+		spaPacketInfor.srcIPv6 = ip.SrcIP.String()
+	} else {
+		spaPacketInfor.srcIPv6 = ""
 	}
-
-	udpLayer := packet.Layer((layers.LayerTypeUDP))
-	if udpLayer != nil {
-		udp, _ := udpLayer.(*layers.UDP)
-		fmt.Printf("From port %d to %d\n", udp.SrcPort, udp.DstPort)
-		fmt.Println()
+	if spaPacketInfor.srcIPv4 == "" && spaPacketInfor.srcIPv6 == "" {
+		return spaPacketInfor, errors.New("can not find ip address")
 	}
-
-	// When iterating through packet.Layers() above,
-	// if it lists Payload layer then that is the same as
-	// this applicationLayer. applicationLayer contains the payload
 	applicationLayer := packet.ApplicationLayer()
 	if applicationLayer != nil {
-		fmt.Println("Application layer/Payload found.")
-		fmt.Printf("%s\n", applicationLayer.Payload())
-
-		// Search for a string inside the payload
-		if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
-			fmt.Println("HTTP found!")
-		}
+		spaPacketInfor.authData = string(applicationLayer.Payload())
 	}
+	return spaPacketInfor, nil
 }
